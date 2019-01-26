6eda3cd2fe6adc6de867f617b6f16547673d49b0
Joseph Deguzman <46972060+jdeguzman1231@users.noreply.github.com>
2019-01-26 17:53:50 -0500

    Add files via upload

---
+++ FullPlannerException.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * 
+ * 
+ * @author Joseph Deguzman
+ * 		email: joseph.deguzman@stonybrook.edu
+ * 		Stony Brook ID: 111720116
+ *
+ */
+public class FullPlannerException extends Exception{
+	
+	/**
+	 * 
+	 * 
+	 * @param message
+	 * 	The output message that displays when the input is called
+	 */
+	public FullPlannerException(String message) 
+	{
+		super(message);
+	}
+}
